f99e5189919c2a08d5fd89b5d478524d42b94e73
Danny <mezenov.201521@gmail.com>
2020-09-11 17:11:50 +0300

    Initial commit

---
+++ src/ru/mirea/TestAuthor.java
@@ -0,0 +1,9 @@
+package ru.mirea;
+
+public class TestAuthor {
+
+    public static void main(String[] args) {
+	    Author author = new Author("Ivan Popov", "ivPopov@somwhere.com", 'm');
+        System.out.println(author.toString());
+    }
+}
